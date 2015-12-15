8ea04205664cece0620a60f4767cd9577d0234e1
Lini Mestar <lmEshoo@users.noreply.github.com>
2015-12-14 22:00:30 -0800

    Create lightOnOff.py

---
+++ lightOnOff.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+LedPin = 7
+littlebitsPin =11
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LedPin, GPIO.OUT)
+GPIO.setup(littlebitsPin, GPIO.OUT)
+
+try:
+    while True:
+        print '...led off'
+        GPIO.output(LedPin, GPIO.LOW)
+        GPIO.output(littlebitsPin, GPIO.LOW)
+        time.sleep(0.5)
+        print 'led on...'
+        GPIO.output(LedPin, GPIO.HIGH)
+        GPIO.output(littlebitsPin, GPIO.HIGH)
+        time.sleep(1.5)
+except KeyboardInterrupt:
+    GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.output(littlebitsPin, GPIO.HIGH)
+
+    GPIO.cleanup()
