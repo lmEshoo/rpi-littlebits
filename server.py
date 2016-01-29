ec7f152c414fd6b796aec447e6bba45f8d4035e3
Lini Mestar <lmEshoo@users.noreply.github.com>
2016-01-29 10:39:58 -0800

    Update server.py

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ app = web.application(urls,globals())
 class root:
 
         def __init__(self):
-                self.hello = "Ready for HackPoly?"
+                self.hello = "snakes on a pie!"
 
         def GET(self):
                 getInput = web.input(turn="")
@@ -47,7 +47,73 @@ class root:
                                 </body>
                                 </html>
                                 """
-               
+                if kommando == "off":
+                        # set RPi board pins low
+                        GPIO.output(7, GPIO.LOW)
+                        GPIO.output(11, GPIO.LOW)
+                        return """<html>
+                                    <head>
+                                    <script>
+                                    function loaded()
+                                    {
+                                        window.setTimeout(CloseMe, 5);
+                                    }
+
+                                    function CloseMe()
+                                    {
+                                        window.close();
+                                    }
+                                    </script>
+                                    </head>
+                                    <body onLoad="loaded()">
+                                    Stop!
+                                    </body>
+                                    </html>
+                            """
+                if kommando == "right":
+                    GPIO.PWM(7, 1000).start(50)
+                    GPIO.output(11, GPIO.HIGH)
+                    return """<html>
+                                    <head>
+                                    <script>
+                                    function loaded()
+                                    {
+                                        window.setTimeout(CloseMe, 5);
+                                    }
+
+                                    function CloseMe()
+                                    {
+                                        window.close();
+                                    }
+                                    </script>
+                                    </head>
+                                    <body onLoad="loaded()">
+                                    right!
+                                    </body>
+                                    </html>
+                            """
+                if kommando == "left":
+                    GPIO.PWM(11, 1000).start(50)
+                    GPIO.output(7, GPIO.HIGH)
+                    return """<html>
+                                    <head>
+                                    <script>
+                                    function loaded()
+                                    {
+                                        window.setTimeout(CloseMe, 5);
+                                    }
+
+                                    function CloseMe()
+                                    {
+                                        window.close();
+                                    }
+                                    </script>
+                                    </head>
+                                    <body onLoad="loaded()">
+                                    left!
+                                    </body>
+                                    </html>
+                            """
 #7 is right 11 is left
 
 if __name__ == "__main__":
